291fc79c9ed3389fe0d6b17d7640aec5309e2f1d
Rasmus Einarsson <rasmus.einarsson@gmail.com>
2015-12-31 11:13:52 +0100

    Add transactions and context manager to fsdb
    Context manager basically just wraps the sqlite3
    manager.
    
    Transactions seem to work but implementation is
    largely untested.

--- gpc/fsdb.py
+++ gpc/fsdb.py
@@ -7,14 +7,6 @@ from gpc import common
 
 from gpc.common import unique_json, hexdigest
 
-def save_stmt(data_dir, stmt):
-    if stmt.lstrip().lower().startswith('insert'):
-        digest = hexdigest(stmt)
-        path = os.path.join(data_dir, digest)
-        if not os.path.exists(path):
-            with open(path, 'w') as file:
-                file.write(stmt)
-                file.write('\n')
 
 
 class Database(object):
@@ -24,6 +16,25 @@ class Database(object):
         self._path = os.path.abspath(path)
         self._data_path = os.path.join(self._path, 'data')
         self._schema_path = os.path.join(self._path, 'schema.sql')
+        self._transaction = []
+
+    def _handle_stmt(self, stmt):
+        if stmt.strip() in ('BEGIN', 'ROLLBACK'):
+            self._transaction = []
+        elif stmt == 'COMMIT':
+            for s in self._transaction:
+                self._write_statement(s)
+        else:
+            self._transaction.append(stmt)
+
+    def _write_statement(self, stmt):
+        if stmt.lstrip().lower().startswith('insert'):
+            digest = hexdigest(stmt)
+            path = os.path.join(self._data_path, digest)
+            if not os.path.exists(path):
+                with open(path, 'w') as file:
+                    file.write(stmt)
+                    file.write('\n')
 
 
     @classmethod
@@ -41,8 +52,7 @@ class Database(object):
                 sql = open(path, 'r').read()
                 conn.execute(sql)
 
-        saver = functools.partial(save_stmt, db._data_path)
-        db._conn.set_trace_callback(saver)
+        db._conn.set_trace_callback(db._handle_stmt)
         return db
 
 
@@ -53,8 +63,13 @@ class Database(object):
         os.makedirs(db._data_path)
         open(db._schema_path, 'w').write(schema)
 
+
+    def __enter__(self, *args, **kwargs):
+        self._conn.__enter__(*args, **kwargs)
+        return self
+
+    def __exit__(self, *args, **kwargs):
+        self._conn.__exit__(*args, **kwargs)
         
     def execute(self, sql, parameters=()):
-        with self._conn:
-            result = self._conn.execute(sql, parameters)
-            return result
+        return self._conn.execute(sql, parameters)
