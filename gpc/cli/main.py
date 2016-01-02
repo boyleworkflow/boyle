59e66633dd7bc5803fe4ce1cae6117bb1e8adf3a
Rasmus Einarsson <rasmus.einarsson@gmail.com>
2016-01-02 12:19:48 +0100

    Add gpc init command

--- gpc/cli/main.py
+++ gpc/cli/main.py
@@ -3,6 +3,10 @@ import click
 from gpc.gpc import *
 from gpc import spec_reader
 from gpc.tests import default_user
+import shutil
+
+DEFAULT_LOG_PATH = 'log'
+DEFAULT_STORAGE_PATH = 'storage'
 
 @click.group()
 def main_group():
@@ -11,12 +15,15 @@ def main_group():
 @main_group.command()
 @click.argument('target', nargs=-1)
 def make(target):
-    '''Make target files. Run necessary calculations to generate the target
+    '''
+    Make target files.
+
+    Run necessary calculations to generate the target
     files. If the target files already exist in cache, simply copy them into
     working directory.'''
     user = default_user
-    log = Log('log', user)
-    storage = Storage('storage')
+    log = Log(DEFAULT_LOG_PATH, user)
+    storage = Storage(DEFAULT_STORAGE_PATH)
     graph = spec_reader.graph_from_spec('gpc.yaml')
 
     runner = Runner(log, storage, graph)
@@ -27,4 +34,23 @@ def make(target):
         print('The file was produced by %i run(s):' % len(responsible_runs))
         for r in responsible_runs:
             print(r)
+
+@main_group.command()
+def init():
+    '''
+    Init in the current directory.
+    '''
+    log_created = False
+    storage_created = False
+    try:
+        Log.create(DEFAULT_LOG_PATH)
+        log_created = True
+        Storage.create(DEFAULT_STORAGE_PATH)
+        storage_created = True
+    except Exception as e:
+        if log_created:
+            shutil.rmtree(DEFAULT_LOG_PATH)
+        if storage_created:
+            shutil.rmtree(DEFAULT_STORAGE_PATH)
+        raise e
 
