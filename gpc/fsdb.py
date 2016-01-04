cd88a76d493cb88bdd17e371b71e35c3fb301828
Rasmus Einarsson <rasmus.einarsson@gmail.com>
2016-01-04 17:28:32 +0100

    Change FSDB implementation and API
    * Forcing users to use transactions.
    * Only writing to disk on database close.
    * Database creation now creates empty database.
    * All operations (update, delete, change schema, etc) now supported
      since save operation uses sqlite database dump.
      (However, git-merging may still create hairy conflicts in such cases...)

--- gpc/fsdb.py
+++ gpc/fsdb.py
@@ -2,10 +2,45 @@ import os
 import json
 import sqlite3
 import functools
+import itertools
 from os.path import abspath
+import logging
+from gpc import hexdigest
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 
 class DatabaseError(Exception): pass
+
+class Transaction(object):
+    """docstring for Transaction"""
+    def __init__(self, connection):
+        super(Transaction, self).__init__()
+        self._conn = connection
+        self._open = False
+
+    def __enter__(self, *args, **kwargs):
+        self._conn.__enter__(*args, **kwargs)
+        return self._conn
+
+    def __exit__(self, *args, **kwargs):
+        self._conn.__exit__(*args, **kwargs)
+
+    def execute(self, sql, parameters=()):
+        if not self._open:
+            raise DatabaseError('Transaction not open')
+        return self._conn.execute(sql, parameters)
+
+    def executemany(self, sql, parameters=()):
+        if not self._open:
+            raise DatabaseError('Transaction not open')
+        return self._conn.executemany(sql, parameters)
+
+    def executescript(self, script):
+        if not self._open:
+            raise DatabaseError('Transaction not open')
+        return self._conn.executescript(script)
 
 
 class Database(object):
@@ -14,57 +49,73 @@ class Database(object):
         """Open a database"""
         super(Database, self).__init__()
         path = abspath(path)
-        self._data_path = Database._data_path(path)
-        self._transaction = []
+        self._schema_dir = Database._get_schema_dir(path)
+        self._data_dir = Database._get_data_dir(path)
+
+        def statements():
+            dirs = (
+                self._schema_dir,
+                self._data_dir)
+
+            for d in dirs:
+                for file in os.listdir(d):
+                    stmt_path = os.path.join(d, file)
+                    with open(stmt_path, 'r') as f:
+                        sql = f.read()
+                    yield sql
 
         try:
-            with open(Database._schema_path(path), 'r') as f:
-                schema = f.read()
-
             self._conn = sqlite3.connect(':memory:')
             with self._conn as conn:
-                conn.executescript(schema)
-
-                for filename in os.listdir(self._data_path):
-                    path = os.path.join(self._data_path, filename)
-                    with open(path, 'r') as f:
-                        sql = f.read()
-                    conn.execute(sql)
+                for stmt in statements():
+                    conn.execute(stmt)
         except Exception as e:
             msg = "Database at '{}' could not be opened".format(path)
             raise DatabaseError(msg) from e
 
-        self._conn.set_trace_callback(self._handle_stmt)
 
     @staticmethod
-    def _data_path(path):
+    def _get_data_dir(path):
         return abspath(os.path.join(path, 'data'))
 
     @staticmethod
-    def _schema_path(path):
-        return abspath(os.path.join(path, 'schema.sql'))
+    def _get_schema_dir(path):
+        return abspath(os.path.join(path, 'schema'))
 
 
-    def _handle_stmt(self, stmt):
-        if stmt.strip() in ('BEGIN', 'ROLLBACK'):
-            self._transaction = []
-        elif stmt == 'COMMIT':
-            for s in self._transaction:
-                self._write_statement(s)
-        else:
-            self._transaction.append(stmt)
-
-    def _write_statement(self, stmt):
-        if stmt.lstrip().lower().startswith('insert'):
+    def _write_to_disk(self):
+        statements = self._conn.iterdump()
+        assert next(statements) == 'BEGIN TRANSACTION;'
+        for stmt in statements:
+            if stmt == 'COMMIT;':
+                break
+            if stmt.lower().startswith('create table'):
+                target_dir = self._schema_dir
+            else:
+                target_dir = self._data_dir
             digest = hexdigest(stmt)
-            path = os.path.join(self._data_path, digest)
+            path = os.path.join(target_dir, digest)
             if not os.path.exists(path):
                 with open(path, 'w') as file:
                     file.write(stmt)
                     file.write('\n')
+        try:
+            next_stmt = next(statements)
+            raise DatabaseError('unexpected statement {}'.format(next_stmt))
+        except StopIteration:
+            pass
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args, **kwargs):
+        self._write_to_disk()
+
+    def transaction(self):
+        return Transaction(self._conn)
 
     @classmethod
-    def create(cls, path, schema):
+    def create(cls, path):
         """
         Create a new database
 
@@ -75,20 +126,5 @@ class Database(object):
         path = abspath(path)
         if os.path.exists(path):
             raise DatabaseError('Path must not exist when creating database!')
-        os.makedirs(path)
-        os.makedirs(Database._data_path(path))
-        open(Database._schema_path(path), 'w').write(schema)
-
-
-    def __enter__(self, *args, **kwargs):
-        self._conn.__enter__(*args, **kwargs)
-        return self
-
-    def __exit__(self, *args, **kwargs):
-        self._conn.__exit__(*args, **kwargs)
-        
-    def execute(self, sql, parameters=()):
-        return self._conn.execute(sql, parameters)
-
-    def executemany(self, sql, parameters=()):
-        return self._conn.executemany(sql, parameters)
+        os.makedirs(Database._get_schema_dir(path))
+        os.makedirs(Database._get_data_dir(path))
