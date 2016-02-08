68e7e12d3c1a5d53018ab3067dde92156bbc8e80
Rasmus Einarsson <rasmus.einarsson@gmail.com>
2016-02-08 12:33:50 +0100

    Clean up/clarify in runner

--- gpc/runner.py
+++ gpc/runner.py
@@ -1,4 +1,4 @@
-from enum import Enum
+import enum
 import logging
 import os
 from gpc import get_calc_id, get_comp_id, NotFoundException
@@ -9,12 +9,13 @@ import shutil
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-
-class Status(Enum):
-    unknown = 0
-    no_digest = 1
-    digest_known = 2
-    file_exists = 3
+@enum.unique
+class Status(enum.IntEnum):
+    inputs_unknown = 0
+    inputs_missing = 1
+    ready_to_run = 2
+    digest_known = 3
+    file_exists = 4
 
 
 class Runner(object):
@@ -25,14 +26,16 @@ class Runner(object):
         self.graph = graph
 
 
-    def get_calc_id(self, task):
+    def _get_calc_id(self, task):
         logger.debug('Getting calc_id\n\ttask: {}'.format(task))
         inp_tasks = {path: self.graph.get_task(path) for path in task.inputs}
-        inp_calc_ids = {p: self.get_calc_id(t) for p, t in inp_tasks.items()}
+        inp_calc_ids = {p: self._get_calc_id(t) for p, t in inp_tasks.items()}
         inp_digests = {p: self.log.get_digest(calc_id, p)
                         for p, calc_id in inp_calc_ids.items()}
 
         calc_id = get_calc_id(task, inp_digests)
+
+        logger.debug('calc_id decided:\n\ttask: {}\n\tcalc_id: {}'.format(task, calc_id))
 
         self.log.save_task(
             task_id=task.id,
@@ -44,16 +47,14 @@ class Runner(object):
             task_id=task.id,
             inputs=inp_digests)
 
-        logger.debug('calc_id decided:\n\ttask: {}\n\tcalc_id: {}'.format(task, calc_id))
-
         return calc_id
 
 
-    def get_comp_id(self, task):
+    def _get_comp_id(self, task):
         input_tasks = [self.graph.get_task(path) for path in task.inputs]
-        subcomp_ids = [self.get_comp_id(task) for task in input_tasks]
+        subcomp_ids = [self._get_comp_id(task) for task in input_tasks]
 
-        calc_id = self.get_calc_id(task)
+        calc_id = self._get_calc_id(task)
         comp_id = get_comp_id(calc_id, subcomp_ids)
 
         self.log.save_composition(
@@ -64,42 +65,53 @@ class Runner(object):
         return comp_id
 
 
+    def _save_request(self, path, request_time):
+            task = self.graph.get_task(path)
+            comp_id = self._get_comp_id(task)
+            digest = self.get_digest(path)
+
+            self.log.save_request(
+                comp_id=comp_id,
+                time=request_time,
+                digest=digest,
+                path=path)
+
+
     def get_digest(self, path):
         task = self.graph.get_task(path)
-        calc_id = self.get_calc_id(task)
+        calc_id = self._get_calc_id(task)
         digest = self.log.get_digest(calc_id, path)
         return digest
 
 
     def get_status(self, path):
         task = self.graph.get_task(path)
-        try:
-            calc_id = self.get_calc_id(task)
-        except NotFoundException:
-            return Status.unknown
+        input_statuses = [self.get_status(p) for p in task.inputs]
+
+        if any((s < Status.digest_known for s in input_statuses)):
+            return Status.inputs_unknown
+
+        calc_id = self._get_calc_id(task)
 
         try:
             digest = self.log.get_digest(calc_id, path)
         except NotFoundException:
-            return Status.no_digest
+            # Output digest does not exist
+            if all((s == Status.file_exists for s in input_statuses)):
+                return Status.ready_to_run
+            else:
+                return Status.inputs_missing
 
+        # If we come this far, the digests exists
         if self.storage.has_file(digest):
             return Status.file_exists
         else:
             return Status.digest_known
 
 
-    def _save_request(self, path, digest):
-        task = self.graph.get_task(path)
-        comp_id = self.get_comp_id(task)
-        self.log.save_request(
-            comp_id=comp_id,
-            time=time.time(),
-            digest=digest,
-            path=path)
-
-
     def ensure_exists(self, *requested_paths):
+        request_time = time.time()
+
         self.graph.ensure_complete()
 
         upstream_paths = self.graph.get_upstream_paths(*requested_paths)
@@ -109,46 +121,51 @@ class Runner(object):
         status_goals = {p: Status.digest_known for p in upstream_paths}
         status_goals.update({p: Status.file_exists for p in requested_paths})
 
-        def get_paths_to_work_on():
-            counter = 0
-            while True:
-                counter += 1
-                logger.info('working round {}'.format(counter))
-                done = True
-                for path in upstream_paths:
-                    status = self.get_status(path)
-                    logger.info('working {} {}'.format(path, status))
-                    if status.value < status_goals[path].value:
-                        logger.info('working on {} {}'.format(path, status))
-                        yield path, status
-                        done = False
-                        break
-                logger.info('end of working round {}, done: {}'.format(counter, done))
-                if done:
-                    break
+        counter = 0
+        work_remains = True
+        while work_remains:
+            work_remains = False # Let's assume until we are proven wrong.
 
-        for path, status in get_paths_to_work_on():
-            task = self.graph.get_task(path)
-            if status in (Status.no_digest, Status.digest_known):
-                all_inputs_exist = True
-                for p in task.inputs:
-                    if self.get_status(p) != Status.file_exists:
-                        all_inputs_exist = False
-                        status_goals[p] = Status.file_exists
+            counter += 1
+            logger.info('Working round {}'.format(counter))
 
-                if all_inputs_exist:
+            for path in upstream_paths:
+                task = self.graph.get_task(path)
+                status = self.get_status(path)
+                if status.value >= status_goals[path].value:
+                    continue
+                else:
+                    work_remains = True
+
+                logger.info('Working on {} ({})'.format(path, status))
+
+                # Working on something with missing inputs means
+                # that the inputs must all exist before continuing:
+                if status == Status.inputs_missing:
+                    status_goals.update(
+                        {p: Status.file_exists for p in task.inputs})
+
+                # Working on something "ready to run" or "digest known"
+                # means that it should be run / rerun
+                elif status in (Status.ready_to_run, Status.digest_known):
                     self._run(task)
-            else:
-                raise RuntimeError(
-                    'Unexpected status {} for {}'.format(status, path))
+
+                else:
+                    raise RuntimeError(
+                        'Unexpected status {} for {}'.format(status, path))
+
+                # Always break (start a new round) after doing something
+                break
 
         for path in requested_paths:
-            digest = self.get_digest(path)
-            self._save_request(path, digest)
+            self._save_request(path, request_time)
 
     def _run(self, task):
-        calc_id = self.get_calc_id(task)
-        logger.info('Running calculation\n\tcalc_id: {}\n\ttask: {}'.format(calc_id, task))
+        calc_id = self._get_calc_id(task)
+        logger.info((
+            'Running calculation\n'
+            '\tcalc_id: {}\n'
+            '\ttask: {}').format(calc_id, task))
 
         # create temp dir
         # run task.prepare(...)
