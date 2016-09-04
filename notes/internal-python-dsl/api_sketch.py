43cbe36d691a853e4f8953485c1c420d27502ba8
Rasmus Einarsson <mr@rasmuseinarsson.se>
2016-09-04 23:36:49 +0200

    Add notes from yesterday's work on Python API/DSL

---
+++ notes/internal-python-dsl/api_sketch.py
@@ -0,0 +1,41 @@
+class Definition:
+    # out: one Resource
+    # inp: zero or more Resource
+    # do: zero or more Operation
+
+class Resource:
+    def digest(): pass
+    def save(): pass # make sure it can be restored
+    def release(): pass ???
+    def restore(digest): pass # place in working dir
+    def represent(digest): pass
+
+    name = None
+    is_variable = False
+
+
+
+class File:
+    def restore(digest):
+        # assume that os.getcwd() is where the operation happens
+        path = os.path.join(os.getcwd(), self.rel_path)
+        self.file_storage.restore(digest, path)
+    def represent():
+        # assume that os.getcwd() is end user's working dir
+        self.restore()
+
+
+class Operation:
+    pass
+
+
+def define(out):
+
+
+    return Definition(name=out.name)
+
+
+# create temp directory and cd
+# restore all inputs
+# run all operations
+# save output
