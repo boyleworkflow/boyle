c335b2da3a7b1003dbbd48cc51b64997b3cf2f31
Rasmus Einarsson <mr@rasmuseinarsson.se>
2016-09-06 17:07:12 +0200

    Add new test that disappeared
    Apparently did not add the test file in a previous commit.

---
+++ tests/test_boyle_make_api.py
@@ -0,0 +1,32 @@
+import unittest
+import pkg_resources
+import os
+import tempfile
+import shutil
+import logging
+import boyle
+
+logging.basicConfig(level=logging.DEBUG)
+
+class TestBoyle(unittest.TestCase):
+    def setUp(self):
+        self.tempdir = tempfile.mkdtemp()
+
+    def test_file_output(self):
+        filename = 'foo.txt'
+        file_contents = 'bar'
+
+        foo = boyle.define(
+            out=boyle.File(filename),
+            do=boyle.Shell('echo {} > {}'.format(file_contents, filename)))
+
+        boyle.deliver(foo, self.tempdir)
+
+        with open(os.path.join(self.tempdir, filename)) as f:
+            self.assertTrue(f.read() == file_contents)
+
+    def tearDown(self):
+        shutil.rmtree(self.tempdir)
+
+if __name__ == '__main__':
+    unittest.main()
