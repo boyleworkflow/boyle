9bd0952b6ad16816d3e823a65be2120c351afe66
Rasmus Einarsson <mr@rasmuseinarsson.se>
2016-11-20 22:09:52 +0100

    Continue working on implementation sketch

--- notes/internal-python-dsl/implementation_sketch.py
+++ notes/internal-python-dsl/implementation_sketch.py
@@ -1,26 +1,78 @@
-
 class Context:
     user = ...
+    def get_result(calculation, instrument, tmax): pass
 
-def ensure_restorable(requested_defs, ctx):
 
-    run_needed = set()
+def _run(calculation, output_instruments, storage):
+    with Context() as context:
+        for r in calculation.inputs:
+            r.restore(storage, context)
+
+        run_script = '''
+            import boyle
+            procedure = boyle.Procedure.from_json({})
+            procedure()
+            '''.format(calculation.procedure.to_json())
+        subprocess.run('python -c "{}"'.format(run_script), context.workdir)
+
+        results = {
+            instrument: instrument.save(context, storage)
+            for instrument in output_instruments
+            }
+
+        return results
+
+
+
+# Resources should probably be orthogonal in most cases.
+# But this is hard to guarantee since they are allowed to change things
+# around them as they are loaded into a context. So let's decide
+# that they are ordered.
+
+# context/workdir
+# context/resources/af388c.../resource.json
+# context/resources/af388c.../data/...
+
+# script = '''
+#     import boyle
+#     inp = boyle.load_resources()
+#     {{ user_script }}
+#     {% for vi in value_instruments %}
+#     boyle.save_value({{ vi.name }}, {{ vi.name }})
+#     {% endfor %}
+#     '''
+
+# octave_script = '''
+#     pkg load boyle
+#     {% for vr in value_resources_in %}
+#     inp.{{ vr.instrument.name }} = loadvalue('{{ vr.digest }}')
+#     {% endfor %}
+#     {{ user_octave_script }}
+#     {% for instr in out_instruments %}
+#     savevalue({% instr.name %})
+#     {% endfor %}
+#     '''
+
+def ensure_restorable(requested_defs, storage):
+
+    run_needed = {}
     while True:
-        for calc in run_needed:
-            run(calc, ctx)
+        for calc, instruments in run_needed.items():
+            _run(calc, mommy)
         try:
-            return _resolve(requested_defs, ctx)
+            return _resolve(requested_defs, log, storage)
         except RunRequired as e:
             run_needed = e.calculations
 
 
-def _resolve(requested_defs, ctx):
+def _resolve(requested_defs, log, storage):
     """
     Tries to resolve the Resources defined by Definitions defs.
 
     Args:
         requested_defs: An iterable of requested Definitions.
-        ctx: The "home" Context to start the work from.
+        log: A Log to read from and write in.
+        storage: A Storage for resources.
 
     Returns:
         A dictionary of {definition: resource} pairs.
@@ -42,7 +94,7 @@ def _resolve(requested_defs, ctx):
             [get_result(inp) for inp in definition.inputs])
 
     def get_result(definition):
-        ctx.get_result(
+        log.get_result(
             calculation=get_calculation(definition),
             instrument=definition.instrument,
             tmax=time,
@@ -51,19 +103,20 @@ def _resolve(requested_defs, ctx):
     requested_defs = set(requested_defs)
     defs = get_upstream_sorted(requested_defs)
 
-    run_needed = set()
+    run_needed = defaultdict(set)
     for d in defs:
-        if run_needed.intersection(d.inputs):
+        if d.inputs.intersection(set(run_needed)):
             raise RunRequired(run_needed)
 
         calc = get_calculation(d)
         try:
             resource = get_result(d)
         except NotFound:
-            run_needed.add(calc)
+            run_needed[calc].add(d.instrument)
             continue
 
-        if minimum_progress[d] in requested_defs and not ctx.can_restore(d):
-            run_needed.add(d)
+        if d in requested_defs and not resource.can_restore_from(storage):
+            run_needed[calc].add(d.instrument)
 
+    assert not run_needed
     return {d: get_result(d) for d in requested_defs}
