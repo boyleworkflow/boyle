15f94434ae733de33373d87d6454695a8bc86d36
Rasmus Einarsson <mr@rasmuseinarsson.se>
2017-03-12 12:56:22 +0100

    Start sketching new implementation

--- notes/internal-python-dsl/implementation_sketch.py
+++ notes/internal-python-dsl/implementation_sketch.py
@@ -1,126 +1,150 @@
-class Context:
-    user = ...
-    def get_result(calculation, instrument, tmax): pass
+import attr
+
+@attr.s
+class ConflictError(Exception):
+    defs = attr.ib()
+
+@attr.s
+class Instrument:
+    def copy(self, src_path, dst_path): pass
+    def digest(self, path): pass # return Digest or raise exception
+    def can_save(self, path, storage): pass # return boolean
+    def save(self, path, storage): pass # return StorageMeta or raise exception
+    def can_restore(self, meta, storage): pass # return boolean
+    def restore(self, meta, storage, path): pass
+
+@attr.s
+class Definition:
+    instrument = attr.ib()
+    parents = attr.ib()
+    operation = attr.ib()
+
+@attr.s
+class Resource:
+    instrument = attr.ib()
+    digest = attr.ib()
+
+@attr.s
+class Operation:
+    pass
+
+@attr.s
+class Calculation:
+    inputs = attr.ib()
+    operation = attr.ib()
+
+@attr.s
+class Result:
+    calculation = attr.ib()
+    resource = attr.ib()
 
 
-def _run(calculation, output_instruments, log, storage):
-    with Context() as context:
-        for r in calculation.inputs:
-            r.restore(storage, context)
-
-        run_script = '''
-            import boyle
-            procedure = boyle.Procedure.from_json({})
-            procedure()
-            '''.format(calculation.procedure.to_json())
-        subprocess.run('python -c "{}"'.format(run_script), context.workdir)
-
-        output_resources = [
-            Resource(instrument, instrument.digest(context))
-            for instrument in output_instruments]
-
-        for r in output_resources:
-            r.save(context, storage)
-
-        # save calculation -> output resources links in log
-        # save run meta data in log (execution time, etc, etc)
+def _get_ancestors(defs):
+    defs = set()
+    new_defs = set(defs)
+    while new_defs:
+        defs.update(new_defs)
+        new_defs = set.union(*(d.parents for d in new_defs)) - defs
+    return defs
 
 
+@attr.s
+class Scheduler:
 
+    log = attr.ib()
+    storage = attr.ib()
 
-# Resources should probably be orthogonal in most cases.
-# But this is hard to guarantee since they are allowed to change things
-# around them as they are loaded into a context. So let's decide
-# that they are ordered.
-
-# context/workdir
-# context/resources/af388c.../resource.json
-# context/resources/af388c.../data/...
-
-# script = '''
-#     import boyle
-#     inp = boyle.load_resources()
-#     {{ user_script }}
-#     {% for vi in value_instruments %}
-#     boyle.save_value({{ vi.name }}, {{ vi.name }})
-#     {% endfor %}
-#     '''
-
-# octave_script = '''
-#     pkg load boyle
-#     {% for vr in value_resources_in %}
-#     inp.{{ vr.instrument.name }} = loadvalue('{{ vr.digest }}')
-#     {% endfor %}
-#     {{ user_octave_script }}
-#     {% for instr in out_instruments %}
-#     savevalue({% instr.name %})
-#     {% endfor %}
-#     '''
-
-def ensure_restorable(requested_defs, log, storage):
-
-    run_needed = {}
-    while True:
-        for calc, instruments in run_needed.items():
-            _run(calc, instruments, log, storage)
+    def _can_restore(resource):
         try:
-            return _resolve(requested_defs, log, storage)
-        except RunRequired as e:
-            run_needed = e.calculations
-
-
-def _resolve(requested_defs, log, storage):
-    """
-    Tries to resolve the Resources defined by Definitions defs.
-
-    Args:
-        requested_defs: An iterable of requested Definitions.
-        log: A Log to read from and write in.
-        storage: A Storage for resources.
-
-    Returns:
-        A dictionary of {definition: resource} pairs.
-
-    Raises:
-        RunRequired, if additional calculations must be run before
-        the defs can be resolved.
-    """
-
-    # Mathematically this is seen as a function mapping
-    # (Time, User, Definition) -> Resource | Requirement[]
-    # So the first thing to note is
-    # time = now   
-    # user = ctx.user
-
-    def get_calculation(definition):
-        return Calculation(
-            definition.procedure,
-            [get_result(inp) for inp in definition.inputs])
-
-    def get_result(definition):
-        log.get_result(
-            calculation=get_calculation(definition),
-            instrument=definition.instrument,
-            tmax=time,
-            )
-
-    requested_defs = set(requested_defs)
-    defs = get_upstream_sorted(requested_defs)
-
-    run_needed = defaultdict(set)
-    for d in defs:
-        if d.inputs.intersection(set(run_needed)):
-            raise RunRequired(run_needed)
-
-        calc = get_calculation(d)
-        try:
-            resource = get_result(d)
+            storage_meta = self.log.get_storage_meta(resource)
         except NotFound:
-            run_needed[calc].add(d.instrument)
-            continue
+            return False
+        return resource.instrument.can_restore(storage_meta, self.storage)
 
-        if d in requested_defs and not resource.can_restore_from(storage):
-            run_needed[calc].add(d.instrument)
+    def _determine_sets(self, defs):
+        defs = topological_sort(defs)
 
-    assert not run_needed
-    return {d: get_result(d) for d in requested_defs}
+        sets = {
+            name: set() for name in (
+                'Concrete',
+                'Abstract',
+                'Known',
+                'Unknown',
+                'Conflict',
+                'Restorable',
+                'Runnable',
+                )
+            }
+
+        for d in defs:
+            if d.parents <= sets['Known']:
+                sets['Concrete'].add(d)
+            else:
+                sets['Abstract'].add(d)
+                continue
+
+            if d.parents <= sets['Restorable']:
+                sets['Runnable'].add(d)
+
+            trusted_results = self.log.get_trusted_results(d)
+            num_trusted = len(trusted_results)
+
+            if num_trusted == 0:
+                sets['Unknown'].add(d)
+                continue
+            elif num_trusted > 2:
+                sets['Conflict'].add(d)
+                continue
+            elif num_trusted == 1:
+                sets['Known'].add(d)
+                trusted_result, = trusted_results
+
+            if self._can_restore(trusted_result.resource):
+                sets['Restorable'].add(d)
+
+        return sets
+    
+    def _get_ready_and_needed(self, requested):
+        defs = _get_ancestors(requested)
+        sets = self._determine_sets(defs)
+
+        if sets['Conflict']:
+            raise ConflictError(sets['Conflict'])
+
+        if requested <= sets['Restorable']:
+            return set()
+
+        candidates = set()
+        additional = (requested - sets['Restorable']).union(sets['Unknown'])
+
+        while additional:
+            candidates.update(additional)
+            additional = (
+                set.union(*(d.parents for d in candidates))
+                - sets['Restorable']
+                )
+
+        final = candidates.intersection(sets['Runnable'])
+        assert len(final) > 0
+        return final
+
+    def _ensure_restorable(requested, path, log):
+        while True:
+            defs_to_run = self._get_ready_and_needed(requested)
+            if not defs_to_run:
+                break
+
+            for d in defs_to_run:
+                run_def(d)
+
+        
+
+class Log:
+
+    def get_trusted_results(calculation, instrument):
+        raise NotImplementedError()
+
+    def get_storage_meta(resource):
+        raise NotImplementedError()
+
+
---
+++ notes/internal-python-dsl/implementation_sketch_old.py
@@ -0,0 +1,126 @@
+class Context:
+    user = ...
+    def get_result(calculation, instrument, tmax): pass
+
+
+def _run(calculation, output_instruments, log, storage):
+    with Context() as context:
+        for r in calculation.inputs:
+            r.restore(storage, context)
+
+        run_script = '''
+            import boyle
+            procedure = boyle.Procedure.from_json({})
+            procedure()
+            '''.format(calculation.procedure.to_json())
+        subprocess.run('python -c "{}"'.format(run_script), context.workdir)
+
+        output_resources = [
+            Resource(instrument, instrument.digest(context))
+            for instrument in output_instruments]
+
+        for r in output_resources:
+            r.save(context, storage)
+
+        # save calculation -> output resources links in log
+        # save run meta data in log (execution time, etc, etc)
+
+
+
+
+# Resources should probably be orthogonal in most cases.
+# But this is hard to guarantee since they are allowed to change things
+# around them as they are loaded into a context. So let's decide
+# that they are ordered.
+
+# context/workdir
+# context/resources/af388c.../resource.json
+# context/resources/af388c.../data/...
+
+# script = '''
+#     import boyle
+#     inp = boyle.load_resources()
+#     {{ user_script }}
+#     {% for vi in value_instruments %}
+#     boyle.save_value({{ vi.name }}, {{ vi.name }})
+#     {% endfor %}
+#     '''
+
+# octave_script = '''
+#     pkg load boyle
+#     {% for vr in value_resources_in %}
+#     inp.{{ vr.instrument.name }} = loadvalue('{{ vr.digest }}')
+#     {% endfor %}
+#     {{ user_octave_script }}
+#     {% for instr in out_instruments %}
+#     savevalue({% instr.name %})
+#     {% endfor %}
+#     '''
+
+def ensure_restorable(requested_defs, log, storage):
+
+    run_needed = {}
+    while True:
+        for calc, instruments in run_needed.items():
+            _run(calc, instruments, log, storage)
+        try:
+            return _resolve(requested_defs, log, storage)
+        except RunRequired as e:
+            run_needed = e.calculations
+
+
+def _resolve(requested_defs, log, storage):
+    """
+    Tries to resolve the Resources defined by Definitions defs.
+
+    Args:
+        requested_defs: An iterable of requested Definitions.
+        log: A Log to read from and write in.
+        storage: A Storage for resources.
+
+    Returns:
+        A dictionary of {definition: resource} pairs.
+
+    Raises:
+        RunRequired, if additional calculations must be run before
+        the defs can be resolved.
+    """
+
+    # Mathematically this is seen as a function mapping
+    # (Time, User, Definition) -> Resource | Requirement[]
+    # So the first thing to note is
+    # time = now   
+    # user = ctx.user
+
+    def get_calculation(definition):
+        return Calculation(
+            definition.procedure,
+            [get_result(inp) for inp in definition.inputs])
+
+    def get_result(definition):
+        log.get_result(
+            calculation=get_calculation(definition),
+            instrument=definition.instrument,
+            tmax=time,
+            )
+
+    requested_defs = set(requested_defs)
+    defs = get_upstream_sorted(requested_defs)
+
+    run_needed = defaultdict(set)
+    for d in defs:
+        if d.inputs.intersection(set(run_needed)):
+            raise RunRequired(run_needed)
+
+        calc = get_calculation(d)
+        try:
+            resource = get_result(d)
+        except NotFound:
+            run_needed[calc].add(d.instrument)
+            continue
+
+        if d in requested_defs and not resource.can_restore_from(storage):
+            run_needed[calc].add(d.instrument)
+
+    assert not run_needed
+    return {d: get_result(d) for d in requested_defs}
