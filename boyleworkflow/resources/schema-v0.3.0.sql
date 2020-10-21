1d6708ffb92a1335273eaf85cf458ce73e0dfcf3
Rasmus Einarsson <mr@rasmuseinarsson.se>
2020-10-21 17:42:08 +0200

    Tweak database schema

--- boyleworkflow/resources/schema-v0.3.0.sql
+++ boyleworkflow/resources/schema-v0.3.0.sql
@@ -23,27 +23,32 @@ create table run (
   foreign key calc_id references calc(calc_id)
 ) without rowid;
 
--- Each result of a calculation is a file or dir placed at a given path.
+-- Each result of a calculation is a Tree.
 create table result (
   run_id text not null,
-  result_path text not null,
-  tree_item_type text not null,
-  tree_item_id text not null,
-  trusted boolean,
+  out_loc text not null,
+  tree_id text not null,
   foreign key run_id references run(run_id),
-  primary key (run_id, result_path)
+  primary key (run_id, out_loc)
 ) without rowid;
+
+create table trust (
+  run_id text not null,
+  out_loc text not null,
+  opinion boolean not null,
+  foreign key (run_id, out_loc) references result(run_id, out_loc),
+  primary key (run_id, out_loc)
+);
 
 create table provenance (
   workflow_blob_id text not null,
   workflow_input_tree_id text not null,
   target_key text not null,
   request_time timestamp not null,
-  calc_id text not null,
   result_path text not null,
-  tree_item_type text not null,
-  tree_item_id text not null,
+  result_tree_id text not null,
   foreign key workflow_input_tree_id references tree_item(tree_id),
+  foreign key result_tree_id references tree_item(tree_id),
   foreign key calc_id references calc_item(calc_id),
   primary key (workflow_blob_id, workflow_input_tree_id, target_key, request_time)
 ) without rowid;
