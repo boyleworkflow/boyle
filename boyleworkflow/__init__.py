4eec092e6f1a85a3d794d47a2344509ad3fdb623
Rasmus Einarsson <mr@rasmuseinarsson.se>
2020-12-05 23:40:04 +0100

    Define run calc steps

--- boyleworkflow/__init__.py
+++ boyleworkflow/__init__.py
@@ -1,1 +1,43 @@
-__version__ = '0.2.0'
+__version__ = "0.2.0"
+
+from dataclasses import dataclass
+from typing import Any, Generic, Iterable, Mapping, Protocol, TypeVar
+
+
+class Op(Protocol):
+    ...
+
+
+Sandbox = TypeVar("Sandbox")
+
+CalcInput = Any
+
+
+@dataclass
+class Calc:
+    inp: Iterable[CalcInput]
+    op: Op
+
+
+class Env(Generic[Sandbox], Protocol):
+    def run_op(self, op: Op, sandbox: Sandbox):
+        ...
+
+    def create_sandbox(self) -> Sandbox:
+        ...
+
+    def destroy_sandbox(self, sandbox: Sandbox):
+        ...
+
+    def place(self, item: CalcInput, sandbox: Sandbox):
+        ...
+
+
+def run(calc: Calc, env: Env):
+    sandbox = env.create_sandbox()
+    try:
+        for item in calc.inp:
+            env.place(item, sandbox)
+        env.run_op(calc.op, sandbox)
+    finally:
+        env.destroy_sandbox(sandbox)
---
+++ tests/test_run_calc.py
@@ -0,0 +1,47 @@
+import pytest
+from pytest import fixture
+from unittest.mock import Mock, call
+from boyleworkflow import run, Calc
+
+
+@fixture
+def calc():
+    return Calc(["a", "b", "c"], Mock())
+
+
+def test_creates_exactly_one_sandbox(calc):
+    env = Mock()
+    run(calc, env)
+    env.create_sandbox.assert_called_once()
+
+
+def test_runs_op_in_sandbox(calc):
+    env = Mock()
+    sandbox = env.create_sandbox()
+    run(calc, env)
+    env.run_op.assert_called_with(calc.op, sandbox)
+
+
+def test_places_inputs_in_sandbox(calc):
+    env = Mock()
+    sandbox = env.create_sandbox()
+    run(calc, env)
+    expected_calls = [call(item, sandbox) for item in calc.inp]
+    assert expected_calls == env.place.call_args_list
+
+
+def test_destroys_sandbox_after_finishing(calc):
+    env = Mock()
+    sandbox = env.create_sandbox()
+    run(calc, env)
+    env.destroy_sandbox.assert_called_once_with(sandbox)
+
+
+def test_destroys_sandbox_after_failed_run(calc):
+    env = Mock(
+        run_op=Mock(side_effect=Exception()),
+    )
+    sandbox = env.create_sandbox()
+    with pytest.raises(Exception):
+        run(calc, env)
+    env.destroy_sandbox.assert_called_once_with(sandbox)
