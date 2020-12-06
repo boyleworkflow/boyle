b65e7c9197e4fe4b1febb347dbd1f20bdac039e4
Rasmus Einarsson <mr@rasmuseinarsson.se>
2020-12-06 23:45:13 +0100

    Start building GraphState

--- boyleworkflow/graph.py
+++ boyleworkflow/graph.py
@@ -1,7 +1,8 @@
 import itertools
 from boyleworkflow.calc import Loc, Op
+import dataclasses
 from dataclasses import dataclass
-from typing import Collection, Iterable, Mapping, Set, Tuple
+from typing import Collection, FrozenSet, Iterable, Mapping, NewType, Set, Tuple
 
 
 @dataclass(frozen=True)
@@ -31,3 +32,65 @@ def _iter_nodes_and_ancestors(nodes: Iterable[Node]) -> Iterable[Node]:
 
 def get_root_nodes(*nodes: Node) -> Set[Node]:
     return {n for n in _iter_nodes_and_ancestors(nodes) if not n.inp}
+
+
+Digest = NewType("Digest", str)
+
+
+@dataclass
+class GraphState:
+    all_nodes: FrozenSet[Node]
+    requested: FrozenSet[Node]
+    parents_known: FrozenSet[Node]
+    known: FrozenSet[Node]
+    runnable: FrozenSet[Node]
+    restorable: FrozenSet[Node]
+    priority_work: FrozenSet[Node]
+    results: Mapping[Node, Digest]
+
+    @classmethod
+    def from_requested(cls, requested: Iterable[Node]) -> "GraphState":
+        requested = frozenset(requested)
+        root_nodes = get_root_nodes(*requested)
+        return GraphState(
+            all_nodes=frozenset(_iter_nodes_and_ancestors(requested)),
+            requested=requested,
+            parents_known=frozenset(root_nodes),
+            known=frozenset(),
+            runnable=frozenset(root_nodes),
+            restorable=frozenset(),
+            priority_work=frozenset(root_nodes),
+            results={},
+        )
+
+    def _update(self, **changes):
+        return dataclasses.replace(self, **changes)
+
+    def add_results(self, results: Mapping[Node, Digest]):
+        new_results = {**self.results, **results}
+        new_known = self.known.union(new_results)
+        new_parents_known = frozenset(
+            node for node in self.all_nodes if node.parents <= new_known
+        )
+        return self._update(
+            results=new_results,
+            known=new_known,
+            parents_known=new_parents_known,
+        )
+
+    def add_restorable(self, nodes: Iterable[Node]):
+        addition = set(nodes)
+        added_but_not_known = addition - self.known
+        if added_but_not_known:
+            raise ValueError(
+                "cannot the following set restorable "
+                f"because they are not known: {added_but_not_known}"
+            )
+        new_restorable = self.restorable | addition
+        new_runnable = frozenset(
+            node for node in self.all_nodes if node.parents <= new_restorable
+        )
+        return self._update(
+            restorable=new_restorable,
+            runnable=new_runnable,
+        )
--- tests/test_graph.py
+++ tests/test_graph.py
@@ -4,12 +4,22 @@ import pytest
 from pytest import fixture
 from unittest.mock import Mock, call
 from boyleworkflow.calc import Loc
-from boyleworkflow.graph import Node, get_root_nodes
+from boyleworkflow.graph import Digest, GraphState, Node, get_root_nodes
 
 
 @dataclass(frozen=True)
 class MockOp:
     name: Optional[str] = None
+
+
+@fixture
+def root_node():
+    return Node({}, MockOp(), Loc("out"))
+
+
+@fixture
+def derived_node(root_node):
+    return Node({Loc("a"): root_node}, MockOp(), Loc("out"))
 
 
 def build_node_network(
@@ -71,3 +81,88 @@ def test_root():
     assert get_root_nodes(nodes["root1"]) == {nodes["root1"]}
     assert get_root_nodes(nodes["bottom1"]) == {nodes["root1"]}
     assert get_root_nodes(nodes["bottom2"]) == {nodes["root1"], nodes["root2"]}
+
+
+def test_root_node_runnable_on_init(root_node):
+    state = GraphState.from_requested([root_node])
+    assert state.runnable == {root_node}
+
+
+def test_root_node_parents_known_on_init(root_node):
+    state = GraphState.from_requested([root_node])
+    assert state.parents_known == {root_node}
+
+
+def test_root_node_not_known_on_init(root_node):
+    state = GraphState.from_requested([root_node])
+    assert state.known == set()
+
+
+def test_root_node_not_restorable_on_init(root_node):
+    state = GraphState.from_requested([root_node])
+    assert state.restorable == set()
+
+
+def test_root_node_priority_work_on_init(root_node):
+    state = GraphState.from_requested([root_node])
+    assert state.priority_work == {root_node}
+
+
+def test_root_node_is_all_nodes(root_node):
+    state = GraphState.from_requested([root_node])
+    assert state.all_nodes == {root_node}
+
+
+def test_no_results_on_init(root_node):
+    state = GraphState.from_requested([root_node])
+    assert not state.results
+
+
+def test_can_add_results(root_node):
+    state = GraphState.from_requested([root_node])
+    results = {root_node: Mock()}
+    updated = state.add_results(results)
+    assert updated.results == results
+
+
+def test_known_after_adding_result(root_node):
+    state = GraphState.from_requested([root_node])
+    results = {root_node: Mock()}
+    updated = state.add_results(results)
+    assert set(updated.results) == updated.known
+
+
+def test_can_update_restorable(root_node):
+    state = GraphState.from_requested([root_node])
+    results = {root_node: Mock()}
+    updated = state.add_results(results).add_restorable({root_node})
+    assert set(updated.restorable) == {root_node}
+
+
+def test_restorable_must_have_result(root_node):
+    state = GraphState.from_requested([root_node])
+    with pytest.raises(ValueError):
+        state.add_restorable({root_node})
+
+
+def test_derived_and_parent_is_all_nodes(root_node, derived_node):
+    state = GraphState.from_requested([derived_node])
+    assert state.all_nodes == {root_node, derived_node}
+
+
+def test_derived_parents_known_iff_parent_result_added(root_node, derived_node):
+    state = GraphState.from_requested([derived_node])
+    assert derived_node not in state.parents_known
+    results = {root_node: Mock()}
+    with_parent_known = state.add_results(results)
+    assert derived_node in with_parent_known.parents_known
+
+
+def test_runnable_iff_parent_restorable(root_node, derived_node):
+    state = GraphState.from_requested([derived_node])
+    assert derived_node not in state.runnable
+    results = {root_node: Mock()}
+    with_parent_known = state.add_results(results)
+    assert derived_node not in with_parent_known.runnable
+    with_parent_restorable = with_parent_known.add_restorable(results)
+    assert derived_node in with_parent_restorable.runnable
