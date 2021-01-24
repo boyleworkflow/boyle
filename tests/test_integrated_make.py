ea6e2c724004415d09fed235122aed1ca074066f
Rasmus Einarsson <mr@rasmuseinarsson.se>
2021-01-24 21:53:03 +0100

    Make StringFormatEnv work with "directories"
    The previous implementation was a quick hack which won't do
    for nested operations.

--- tests/test_integrated_make.py
+++ tests/test_integrated_make.py
@@ -1,32 +1,58 @@
-from boyleworkflow.tree import Leaf, Path, Tree
+from boyleworkflow.frozendict import FrozenDict
+from tests.util import tree_from_dict
+from boyleworkflow.tree import Leaf, Path, Tree, TreeItem
 from boyleworkflow.calc import SandboxKey
 from dataclasses import dataclass, field
-from typing import Dict, Iterable, MutableMapping, Tuple
+from typing import Dict, Union
 import unittest.mock
 from boyleworkflow.make import make
 from boyleworkflow.nodes import Node, NodeBundle
 
-StringFormatOp = Tuple[Tuple[Path, str], ...]
+StringFormatOp = FrozenDict[str, str]
 
 
 def make_op(**definitions: str) -> StringFormatOp:
-    return tuple((Path.from_string(k), v) for k, v in definitions.items())
+    return FrozenDict(definitions)
 
 
-HELLO_NODE = Node.create({}, make_op(out="Hello"), "out")
-HELLO_WORLD_NODE = Node.create(
-    dict(hello=HELLO_NODE), make_op(out="{hello} World"), "out"
-)
-SIBLING_NODES = NodeBundle.create({}, make_op(a="one", b="two"), ["a", "b"])
+NestedStrDictItem = Union["NestedStrDict", str]
+NestedStrDict = Dict[str, NestedStrDictItem]
+
+PATH_SEP = "/"
+
+
+def place_nested(mapping: NestedStrDict, path: str, value: NestedStrDictItem):
+    *descend_segments, final_segment = path.split(PATH_SEP)
+    for segment in descend_segments:
+        if segment not in mapping:
+            mapping[segment] = {}
+
+        descend_into = mapping[segment]
+        if not isinstance(descend_into, dict):
+            raise ValueError(f"{segment}: {descend_into}")
+        mapping = descend_into
+
+    mapping[final_segment] = value
+
+
+def pick_nested(mapping: NestedStrDict, path: str) -> NestedStrDictItem:
+    *descend_segments, final_segment = path.split(PATH_SEP)
+    for segment in descend_segments:
+        descend_into = mapping[segment]
+        if not isinstance(descend_into, dict):
+            raise ValueError(f"{segment}: {descend_into}")
+        mapping = descend_into
+
+    return mapping[final_segment]
 
 
 @dataclass
 class StringFormatEnv:
-    output: MutableMapping[str, str] = field(default_factory=dict)
+    output: NestedStrDict = field(default_factory=dict)
 
     def __post_init__(self):
-        self._sandboxes: Dict[SandboxKey, Dict[str, str]] = {}
-        self._storage = {}
+        self._sandboxes: Dict[SandboxKey, NestedStrDict] = {}
+        self._storage: Dict[TreeItem, NestedStrDictItem] = {}
 
     def create_sandbox(self):
         sandbox_key = SandboxKey("sandbox")
@@ -38,28 +64,45 @@ class StringFormatEnv:
 
     def run_op(self, op: StringFormatOp, sandbox_key: SandboxKey):
         sandbox = self._sandboxes[sandbox_key]
-        results = {
-            path.to_string(): template.format(**sandbox) for path, template in op
-        }
-        sandbox.update(results)
+        op_results = {path: template.format(**sandbox) for path, template in op.items()}
+        for path, value in op_results.items():
+            place_nested(sandbox, path, value)
 
     def stow(self, sandbox_key: SandboxKey, path: Path):
         sandbox = self._sandboxes[sandbox_key]
-        value = sandbox[path.to_string()]
-        leaf = Leaf(f"leaf:{value}")
-        self._storage[leaf] = value
-        return leaf
+        value = pick_nested(sandbox, path.to_string())
 
-    def _place_into(self, tree: Tree, destination: MutableMapping[str, str]):
-        for path, item in tree.walk():
-            if isinstance(item, Leaf):
-                destination[path.to_string()] = self._storage[item]
+        tree_item = Leaf(value) if isinstance(value, str) else tree_from_dict(value)
+        self._storage[tree_item] = value
+        return tree_item
+
+    def _build_value_from_storage(self, tree_item: TreeItem) -> NestedStrDictItem:
+        if isinstance(tree_item, Leaf):
+            return self._storage[tree_item]
+
+        return {
+            name.value: self._build_value_from_storage(child_item)
+            for name, child_item in tree_item.children.items()
+        }
+
+    def _place_into(self, tree: Tree, destination: NestedStrDict):
+        value = self._build_value_from_storage(tree)
+        if not isinstance(value, dict):
+            raise ValueError(f"expected a dict but found {repr(value)}")
+        destination.update(value)
 
     def place(self, sandbox_key: SandboxKey, tree: Tree):
         self._place_into(tree, self._sandboxes[sandbox_key])
 
     def deliver(self, tree: Tree):
         self._place_into(tree, self.output)
+
+
+HELLO_NODE = Node.create({}, make_op(out="Hello"), "out")
+HELLO_WORLD_NODE = Node.create(
+    dict(hello=HELLO_NODE), make_op(out="{hello} World"), "out"
+)
+SIBLING_NODES = NodeBundle.create({}, make_op(a="one", b="two"), ["a", "b"])
 
 
 def test_make_hello():
