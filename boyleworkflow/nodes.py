932437314792117cff0d4f332994213f41ad2566
Rasmus Einarsson <mr@rasmuseinarsson.se>
2021-01-24 14:31:22 +0100

    Enable node nesting

--- boyleworkflow/nodes.py
+++ boyleworkflow/nodes.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
+from boyleworkflow.tree import Name
 from boyleworkflow.frozendict import FrozenDict
+import dataclasses
 from dataclasses import dataclass
 from boyleworkflow.calc import Path, Op
 from typing import (
@@ -13,6 +15,7 @@ from typing import (
 )
 
 PathLike = Union[Path, str]
+NameLike = Union[Name, str]
 
 # Allowing Collection[PathLike] is possible but opens for mistakes because
 # str is also a Collection[PathLike]. So 'abc' could be confused with ['a', 'b', 'c']
@@ -26,11 +29,19 @@ def _ensure_path(value: PathLike) -> Path:
         return value
 
 
+def _ensure_name(value: NameLike) -> Name:
+    if isinstance(value, str):
+        return Name(value)
+    else:
+        return value
+
+
 @dataclass(frozen=True)
 class NodeBundle:
     inp: FrozenDict[Path, Node]
     op: Op
     out: FrozenSet[Path]
+    levels: Tuple[Name, ...] = ()
 
     @staticmethod
     def create(inp: Mapping[PathLike, Node], op: Op, out: PathLikePlural) -> NodeBundle:
@@ -47,6 +58,17 @@ class NodeBundle:
     def nodes(self: NodeBundle) -> FrozenSet[Node]:
         return frozenset({Node(self, path) for path in self.out})
 
+    def descend(self, level_name: NameLike):
+        converted_name = _ensure_name(level_name)
+        if converted_name in self.levels:
+            raise ValueError(f"duplicate level name {converted_name}")
+        return dataclasses.replace(self, levels=self.levels + (converted_name,))
+
+    def ascend(self):
+        if not self.levels:
+            raise ValueError("cannot ascend non-nested")
+        return dataclasses.replace(self, levels=self.levels[:-1])
+
 
 @dataclass(frozen=True)
 class Node:
@@ -60,3 +82,9 @@ class Node:
     @staticmethod
     def create(inp: Mapping[PathLike, Node], op: Op, out: PathLike) -> Node:
         return NodeBundle.create(inp, op, [out])[out]
+
+    def descend(self, level_name: NameLike):
+        return self.bundle.descend(level_name)[self.out]
+
+    def ascend(self):
+        return self.bundle.ascend()[self.out]
--- tests/test_nodes.py
+++ tests/test_nodes.py
@@ -1,3 +1,5 @@
+import pytest
+from boyleworkflow.tree import Name
 from boyleworkflow.nodes import Node, NodeBundle
 
 
@@ -5,6 +7,10 @@ ROOT1 = Node.create({}, "op1", "loc1")
 ROOT2 = Node.create({}, "op2", "loc2")
 DERIVED = Node.create({"i1": ROOT1, "i2": ROOT2}, "op", "out")
 DERIVED_IDENTICAL = Node.create({"i2": ROOT2, "i1": ROOT1}, "op", "out")
+
+BUNDLE = NodeBundle.create({}, "op", ["out"])
+BUNDLE_L1 = BUNDLE.descend("level1")
+BUNDLE_L2 = BUNDLE_L1.descend("level2")
 
 
 def test_input_loc_affects_hash():
@@ -54,3 +60,52 @@ def test_root_has_no_parents():
 
 def test_derived_has_parents():
     assert DERIVED.parents == {ROOT1, ROOT2}
+
+
+def test_node_bundle_default_not_nested():
+    bundle = NodeBundle.create({}, "op", ["out"])
+    assert bundle.levels == ()
+
+
+def test_node_bundle_descend_does_not_alter():
+    bundle = NodeBundle.create({}, "op", ["out"])
+    bundle.descend("level1")
+    assert bundle.levels == ()
+
+
+def test_node_bundle_descend():
+    assert BUNDLE.descend("level1").levels == (Name("level1"),)
+
+
+def test_node_bundle_descend_twice():
+    assert BUNDLE.descend("l1").descend("l2").levels == (Name("l1"), Name("l2"))
+
+
+def test_node_bundle_descend_levels_must_be_unique():
+    with pytest.raises(ValueError):
+        BUNDLE.descend("level1").descend("level1")
+
+
+def test_node_bundle_ascend():
+    assert BUNDLE_L1.ascend() == BUNDLE
+    assert BUNDLE_L2.ascend() == BUNDLE_L1
+
+
+def test_node_bundle_cannot_ascend_unless_nested():
+    with pytest.raises(ValueError):
+        BUNDLE.ascend()
+
+
+def test_node_descend():
+    bundle = BUNDLE
+    nested_bundle = bundle.descend("level_name")
+
+    node = bundle["out"]
+    nested_node = node.descend("level_name")
+
+    assert nested_bundle["out"] == nested_node
+
+
+def test_node_ascend():
+    node = BUNDLE["out"]
+    assert node.descend("level_name").ascend() == node
