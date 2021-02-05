2650c5ed92cfd8980dd772b449965a542c89d62c
Rasmus Einarsson <mr@rasmuseinarsson.se>
2021-02-05 23:25:23 +0100

    Build simple file Storage

---
+++ boyleworkflow/storage.py
@@ -0,0 +1,110 @@
+from __future__ import annotations
+import os
+from typing import cast
+from boyleworkflow.loc import Loc, Name
+from dataclasses import dataclass
+from pathlib import Path
+import hashlib
+from boyleworkflow.tree import Tree
+
+HASH_NAME = "sha256"
+HASH = getattr(hashlib, HASH_NAME)
+_CHUNK_SIZE = 2 ** 20
+_STORAGE_PATH_SPLIT_LEN = 2
+
+
+def _digest_file(path: Path) -> str:
+    m = HASH()
+
+    with open(path, "rb") as f:
+        while True:
+            data = f.read(_CHUNK_SIZE)
+            if not data:
+                return m.hexdigest()
+            m.update(data)
+
+
+def _describe_file(path: Path) -> Tree:
+    return Tree({}, {HASH_NAME: _digest_file(path)})
+
+
+def _describe_dir(path: Path) -> Tree:
+    return Tree({Name(child.name): describe(child) for child in path.iterdir()})
+
+
+def describe(path: Path) -> Tree:
+    if path.is_dir():
+        return _describe_dir(path)
+    elif path.is_file():
+        return _describe_file(path)
+
+    raise NotImplemented
+
+
+def _represents_file(tree: Tree):
+    return tree.data is not None
+
+
+def _loc_to_rel_path(loc: Loc):
+    path = Path(str(loc))
+    assert not path.is_absolute(), path
+    return path
+
+
+@dataclass
+class Storage:
+    root_path: Path
+
+    def __post_init__(self):
+        if self.root_path.exists():
+            if not self.marker_file_path.exists():
+                raise FileNotFoundError(f"expected to find {self.marker_file_path}")
+        else:
+            self.root_path.mkdir(parents=True)
+            self.marker_file_path.touch()
+
+    @property
+    def marker_file_path(self):
+        return self.root_path / ".boyle-storage"
+
+    def store(self, path: Path) -> Tree:
+        tree = describe(path)
+        self._store_tree(tree, path)
+        return tree
+
+    def _store_tree(self, tree: Tree, start_path: Path):
+        for loc, subtree in tree.walk():
+            if _represents_file(subtree):
+                self._store_file(subtree, start_path / _loc_to_rel_path(loc))
+
+    def _store_file(self, file_tree: Tree, src_path: Path):
+        dst_path = self._get_storage_path(file_tree)
+        dst_path.parent.mkdir(exist_ok=True, parents=True)
+        os.link(src_path, dst_path)
+
+    def restore(self, tree: Tree, path: Path):
+        if _represents_file(tree):
+            self._restore_file(tree, path)
+        else:
+            self._restore_dir(tree, path)
+
+    def _restore_file(self, file_tree: Tree, dst_path: Path):
+        src_path = self._get_storage_path(file_tree)
+        os.link(src_path, dst_path)
+
+    def _restore_dir(self, dir_tree: Tree, dst_path: Path):
+        dst_path.mkdir()
+
+        for name, subtree in dir_tree.items():
+            child_path = dst_path / str(name)
+            self.restore(subtree, child_path)
+
+
+    def _get_storage_path(self, file_tree: Tree) -> Path:
+        hexdigest: str = file_tree.data[HASH_NAME]  # type: ignore
+        return (
+            self.root_path
+            / HASH_NAME
+            / hexdigest[:_STORAGE_PATH_SPLIT_LEN]
+            / hexdigest[_STORAGE_PATH_SPLIT_LEN:]
+        )
---
+++ tests/test_storage.py
@@ -0,0 +1,121 @@
+import pytest
+from boyleworkflow.loc import Name
+from pathlib import Path
+import hashlib
+from typing import Mapping, Union
+from boyleworkflow.storage import Storage, describe
+from boyleworkflow.tree import Tree
+
+FileOrDirSpec = Union[str, Mapping[str, "FileOrDirSpec"]]
+
+ENCODING = "utf-8"
+SOME_TEXT = "some text\n"
+SOME_TEXT_SHA256 = "a23e5fdcd7b276bdd81aa1a0b7b963101863dd3f61ff57935f8c5ba462681ea6"
+
+
+def write_str(s: str, path: Path):
+    with open(path, "w", encoding=ENCODING) as f:
+        f.write(s)
+
+
+def read_str(path: Path) -> str:
+    with open(path, "r", encoding=ENCODING) as f:
+        return f.read()
+
+
+def create_spec_tree(value: FileOrDirSpec) -> Tree:
+    if isinstance(value, str):
+        return Tree({}, {"sha256": value})
+    else:
+        return Tree({Name(k): create_spec_tree(v) for k, v in value.items()})
+
+
+def get_hexdigest(data: bytes) -> str:
+    m = hashlib.sha256()
+    m.update(data)
+    return m.hexdigest()
+
+
+def test_describe_file(tmp_path: Path):
+    file_path = tmp_path / "filename.txt"
+    write_str(SOME_TEXT, file_path)
+
+    expected_spec = create_spec_tree(SOME_TEXT_SHA256)
+    spec = describe(file_path)
+    assert spec == expected_spec
+
+
+def test_describe_nested_dir(tmp_path: Path):
+    subdir_1 = tmp_path / "subdir1"
+    subdir_2 = subdir_1 / "subdir2"
+    file_path = subdir_2 / "filename.txt"
+    subdir_2.mkdir(parents=True)
+    write_str(SOME_TEXT, file_path)
+
+    expected_spec = create_spec_tree(
+        {"subdir1": {"subdir2": {"filename.txt": SOME_TEXT_SHA256}}}
+    )
+    spec = describe(tmp_path)
+    assert spec == expected_spec
+
+
+def test_torage_can_create_dir(tmp_path: Path):
+    storage_root_path = tmp_path / "nonexistent_dir"
+    assert not storage_root_path.exists()
+    Storage(storage_root_path)
+    assert storage_root_path.exists()
+
+
+def test_storage_wont_move_into_empty_dir(tmp_path: Path):
+    storage_root_path = tmp_path / "empty_dir"
+    storage_root_path.mkdir()
+    assert storage_root_path.exists()
+    with pytest.raises(FileNotFoundError):
+        Storage(storage_root_path)
+
+
+def test_can_create_dir_and_then_reopen(tmp_path: Path):
+    storage_root_path = tmp_path / "nonexistent_dir"
+    assert not storage_root_path.exists()
+    Storage(storage_root_path)
+    assert storage_root_path.exists()
+    Storage(storage_root_path)
+
+
+def test_store_describes_file(tmp_path: Path):
+    file_path = tmp_path / "filename.txt"
+    write_str("some text", file_path)
+
+    storage = Storage(tmp_path / "storage")
+    assert storage.store(file_path) == describe(file_path)
+
+
+def test_store_can_restore_file(tmp_path: Path):
+    src_path = tmp_path / "filename.txt"
+    write_str("anything", src_path)
+
+    storage = Storage(tmp_path / "storage")
+    spec = storage.store(src_path)
+
+    restore_path = tmp_path / "other_path.txt"
+    storage.restore(spec, restore_path)
+
+    assert describe(src_path) == describe(restore_path)
+
+
+def test_store_can_restore_nested_dir(tmp_path: Path):
+    subdir_1 = tmp_path / "subdir1"
+    subdir_2 = subdir_1 / "subdir2"
+    file_path = subdir_2 / "filename.txt"
+    subdir_2.mkdir(parents=True)
+    write_str(SOME_TEXT, file_path)
+
+    storage = Storage(tmp_path / "storage")
+    spec = storage.store(subdir_1)
+    print(spec)
+
+    restore_path = tmp_path / "restore_path"
+    storage.restore(spec, restore_path)
+    print(describe(restore_path))
+
+    assert describe(subdir_1) == describe(restore_path)
